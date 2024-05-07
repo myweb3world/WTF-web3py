6ec902f75e434f9e6ca8387e0fc8512edf333104
myweb3world <myweb3world@gmail.com>
2024-05-07 22:04:55 +0800

    添加注释445

--- 01_HelloVitalik/HelloVitalik.py
+++ 01_HelloVitalik/HelloVitalik.py
@@ -1,4 +1,4 @@
 from web3 import Web3
 w3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth'))
 print(w3.is_connected())
-print(w3.eth.get_balance("vitalik.eth")) //333
+print(w3.eth.get_balance("vitalik.eth")) //444
